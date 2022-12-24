a351a93fd2aa6337f99aaadac9a50b0c54c94078
irfan87 <nerve2009@yahoo.com>
2022-12-24 23:59:36 +0800

    style the pages and button component that navigate to the ninja's list

--- pages/index.js
+++ pages/index.js
@@ -3,23 +3,25 @@ import Link from "next/link";
 
 export default function Home() {
 	return (
-		<div>
-			<h1>
+		<div className={styles.container}>
+			<h1 className={styles.title}>
 				Welcome to the Ninja List, where you can search your favorite ninja here
 			</h1>
-			<p>
+			<p className={styles.text}>
 				Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis sed
 				ipsam cupiditate optio dolore, iure, laboriosam similique saepe aliquam
 				fuga illo beatae soluta molestias expedita excepturi, ipsa tenetur
 				quibusdam iste!
 			</p>
-			<p>
+			<p className={styles.text}>
 				Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
 				dolorum, odit blanditiis perspiciatis ab numquam ea voluptatibus
 				dignissimos natus mollitia assumenda explicabo iusto voluptatem
 				repudiandae dolore culpa, repellat itaque autem.
 			</p>
-			<Link href="/ninjas">See other ninjas</Link>
+			<Link href="/ninjas" className={styles.btn}>
+				See other ninjas
+			</Link>
 		</div>
 	);
 }
