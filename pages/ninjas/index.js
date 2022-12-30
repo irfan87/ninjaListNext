f1ec311dfc7d4360b5a3a4a4e245b2bebb4fc9f2
irfan87 <nerve2009@yahoo.com>
2022-12-30 17:04:36 +0800

    fetching the ninjas' data

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,6 +1,18 @@
 import Head from "next/head";
+import Link from "next/link";
 
-const Ninjas = () => {
+import styles from "../../styles/Ninjas.module.css";
+
+export const getStaticProps = async () => {
+	const res = await fetch("https://jsonplaceholder.typicode.com/users");
+	const userJsonData = await res.json();
+
+	return {
+		props: { ninjas: userJsonData },
+	};
+};
+
+const Ninjas = ({ ninjas }) => {
 	return (
 		<>
 			<Head>
@@ -9,6 +21,16 @@ const Ninjas = () => {
 			</Head>
 			<div>
 				<h1>Ninjas List</h1>
+				{ninjas.map((ninja) => (
+					<div key={ninja.id}>
+						<Link
+							className={styles.single}
+							href={`https://jsonplaceholder.typicode.com/users/${ninja.id}`}
+						>
+							<h3>{ninja.name}</h3>
+						</Link>
+					</div>
+				))}
 			</div>
 		</>
 	);
